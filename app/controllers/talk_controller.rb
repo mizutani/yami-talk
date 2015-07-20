ca6e214661aa6e5f7a7269ca983bb067b9cf115a
erefy <erefy.yami.mio@gmail.com>
2015-07-20 22:30:55 +0900

    音声合成処理をバックグラウンド処理に変更

--- app/controllers/talk_controller.rb
+++ app/controllers/talk_controller.rb
@@ -10,18 +10,12 @@ class TalkController < ApplicationController
     text = reqester.get_text.present? ? reqester.get_text : 'テキストの生成に失敗しました。'
 
     file_name = "#{Time.now.strftime('%Y%m%d%H%M%S')}.mp3"
-    converter = Converter::TextToSpeech.new
-    converter.execute(text, create_voice_path(file_name))
-    # CreateVoiceJob.perform_later reqester.get_text, file_name
+    CreateVoiceJob.perform_later text, file_name
 
     render :json => {
       talk_message: text,
       send_message: params[:message],
       file_name: file_name,
     }
-  end
-
-  def create_voice_path(file_name)
-    Rails.root.join("app", "assets", "voices", file_name)
   end
 end
--- app/jobs/create_voice_job.rb
+++ app/jobs/create_voice_job.rb
@@ -7,8 +7,6 @@ class CreateVoiceJob < ActiveJob::Base
   end
 
   def perform(text, file_name)
-  　# TODO 緊急回避 あとでちゃんとした対応を行う
-    text = 'テキストの生成に失敗しました。' if text.blank?
     converter = Converter::TextToSpeech.new
     converter.execute(text, create_voice_path(file_name))
   end
